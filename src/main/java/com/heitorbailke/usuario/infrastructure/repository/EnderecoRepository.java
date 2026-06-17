a91f2d265c0865aeffaeb12b4cd5dcc0d0c7d2b0
Heitor Bailke de Bem <Heitorbailkedev@hotmail.com>
2026-06-16 22:28:34 -0300

    Revert "Feature/atualizacoes"

--- src/main/java/com/heitorbailke/usuario/infrastructure/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.heitorbailke.usuario.infrastructure.repository;
-
-import com.heitorbailke.usuario.infrastructure.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
